4b15c4ed8fad0d55de84b4882cfe450870d932c1
Justas Palumickas <jpalumickas@gmail.com>
2021-10-01 15:36:56 +0300

    Downgrade React

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import UI from './app';
 import getPackage from './utils/getPackage';
 
 const program = new Command();
-program.version('0.1.1');
+program.version('0.1.2');
 
 program.option('-d, --debug', 'Output more logs');
 
